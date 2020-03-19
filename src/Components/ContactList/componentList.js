b2ea112860959620d1dc5403749bb7192da4d4a0
Ihor <igorechekqwerty@gmail.com>
2020-03-20 01:49:23 +0200

    Star change from item in App

--- src/Components/ContactList/componentList.js
+++ src/Components/ContactList/componentList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React  from 'react';
 import './componentList.css';
 import  UserItem from './ContactItems/componentItem.js';
 
-const UserList = () => {
+const UserList = ({List, setFavorite}) => {
+    const item = List.map(item => {
+        return (<UserItem
+        key = {item.id} 
+        name = {item.name}
+        address = {item.address}
+        email = {item.email}
+        phone = {item.phone}
+        avatar = {item.avatar}
+        gender = {item.gender}
+        favorite = {item.favorite}
+        setFavorite = {() => setFavorite(item.id)}
+        />
+        );
+    });
     return (
         <div className="container d-block">
             <ul>
-                <li className="list-group-item"><UserItem /></li>
+                <li className="list-group-item">{ item }</li>
             </ul>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,23 +1,86 @@
 import React from 'react';
-import { Fragment } from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import UserList from './Components/ContactList/componentList';
-
-const App = () => {
+class App extends React.Component {
+    state = {
+        List: [
+            {
+                id: 1,
+                name: "Oleg Vunnuk",
+                address: "Logovo Vovchutsi",
+                phone: "(097)-714-8701",
+                email: "truevunnuk@ukr.net",
+                avatar: 88,
+                gender: 'men',
+                favorite: false
+            },
+            {
+                id:2,
+                name: "John Snow",
+                address: "Castle Black",
+                phone: "(096)-711-5602",
+                email: "truetargarien@ukr.net",
+                avatar: 70,
+                gender: 'men',
+                favorite: false
+            },
+            {
+                id:3,
+                name: "John Wick",
+                address: "1st WallStreet Court, Continental, room number 44",
+                phone: "(099)-000-0099",
+                email: "ilovemydog@ukr.net",
+                avatar: 55,
+                gender: 'men',
+                favorite: false
+            },
+            {
+                id:4,
+                name: "Cleopatra",
+                address: "Cairo",
+                phone: "none",
+                email: "none",
+                avatar: 1,
+                gender: 'women',
+                favorite: false
+            },
+            {
+                id: 5,
+                name: "Spider Man",
+                address: "Forest Hills 20, NY",
+                phone: "(093)-123-4567",
+                email: "bigpowerisbigresponsibility@ukr.net",
+                avatar: 65,
+                gender: 'men',
+                favorite: false
+            }
+        ]
+    }
+    setFavorite = id  => {
+        this.setState (state => {
+            const index = this.state.List.findIndex(elem=> elem.id === id);
+            const tmpList = this.state.List.slice();
+            tmpList[index].favorite = !tmpList[index].favorite;
+            return {
+            favorite: !this.tmpList
+            };
+        });    
+    };
+    render(){
     return (
-        <Fragment>
-            <div className="container">
-                <div className="container d-flex justify-content-center">
-                    <h1>♥ Contact list IT-Step ♥</h1>
-                </div>
-                <div>
-                    <UserList />
-                </div>
+        <div className="container">
+            <div className="container d-flex justify-content-center">
+                <h1>♥ Contact list IT-Step ♥</h1>
             </div>
-        </Fragment>
+            <div>
+            <UserList
+                List={this.state.List}
+                setFavorite = {this.setFavorite}>  
+            </UserList>
+            </div>
+        </div>
     );
+    };
 };
-// state
-// function, class based component
 ReactDOM.render(<App />, document.getElementById('root'));
